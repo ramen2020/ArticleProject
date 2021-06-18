eaabf3476c8da0229a50b178477ac6f207853396
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-06-19 08:41:53 +0900

    fix TableviewCell

--- tonkotsu_ramen/View/ArticleTableViewCell.swift
+++ tonkotsu_ramen/View/ArticleTableViewCell.swift
@@ -9,10 +9,10 @@ import UIKit
 
 class ArticleTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var articleTitle: UILabel!
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var userImage: UIImageView!
-
+    @IBOutlet weak var articleTitle: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,6 +22,19 @@ class ArticleTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func configure(article: Article){
+            
+        let imageUrl = article.user.profile_image_url
+        let url = URL(string: imageUrl)
+        self.userImage.kf.setImage(with: url)
+        
+        let userName = article.user.name.count == 0 ? "名無さん" : article.user.name
+        self.userName.text = userName
+
+        let articleTitle = article.title
+        self.articleTitle.text = articleTitle
     }
     
 }
