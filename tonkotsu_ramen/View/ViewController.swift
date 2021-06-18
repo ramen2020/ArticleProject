f1ca85021a635d707e6f1c049e872904d918f7c6
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-06-19 08:41:41 +0900

    fix viewController

--- tonkotsu_ramen/View/ViewController.swift
+++ tonkotsu_ramen/View/ViewController.swift
@@ -17,37 +17,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchTextField: UITextField!
     
+    private var articleViewModel: ArticleViewModel = ArticleViewModel()
     private let disposeBag = DisposeBag()
-    private var articleViewModel: ArticleViewModel!
     
     override func viewDidLoad() {
-        articleViewModel = ArticleViewModel()
-        self.tableView.register(UINib(nibName: "ArticleTableViewCell", bundle: nil), forCellReuseIdentifier: "ArticleTableViewCell")
         
-        tableView.delegate = self
+        setUpTableView()
         
         searchTextField.rx.text.orEmpty
             .filter { $0.count >= 1 }   
             .bind(to: articleViewModel.input.searchWord)
             .disposed(by: disposeBag)
         
-                articleViewModel.output.articles
-                    .bind(to: tableView.rx.items) { tableView, row, element in
-                        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell")!
-        
-                        let imageView = cell.contentView.viewWithTag(3) as! UIImageView
-                        let url = URL(string: element.user.profile_image_url)!
-                        imageView.kf.setImage(with: url)
-        
-                        let labelTitle = cell.viewWithTag(1) as! UILabel
-                        labelTitle.text = element.title
-        
-                        let labelName = cell.viewWithTag(2) as! UILabel
-                        labelName.text = element.user.name
-        
-                        return cell
-                    }
-                    .disposed(by: disposeBag)
+        articleViewModel.output.articles
+            .bind(to: tableView.rx.items) { tableView, row, element in
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell")
+                        as? ArticleTableViewCell else {
+                    return UITableViewCell()
+                }
+                cell.selectionStyle = UITableViewCell.SelectionStyle.none
+                cell.configure(article: element)
+                return cell
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func setUpTableView() {
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "ArticleTableViewCell", bundle: nil),
+                                forCellReuseIdentifier: "ArticleTableViewCell")
+        tableView.separatorStyle = .none
     }
 }
 
