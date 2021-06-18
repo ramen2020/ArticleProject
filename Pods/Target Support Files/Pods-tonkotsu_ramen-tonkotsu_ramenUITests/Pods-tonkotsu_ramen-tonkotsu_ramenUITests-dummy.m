8a4a9f3001c4cbedc806c716ec03667c94385687
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-06-19 08:01:23 +0900

    add gitignore

--- Pods/Target Support Files/Pods-tonkotsu_ramen-tonkotsu_ramenUITests/Pods-tonkotsu_ramen-tonkotsu_ramenUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tonkotsu_ramen_tonkotsu_ramenUITests : NSObject
-@end
-@implementation PodsDummy_Pods_tonkotsu_ramen_tonkotsu_ramenUITests
-@end
--- Pods/Target Support Files/Pods-tonkotsu_ramenTests/Pods-tonkotsu_ramenTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tonkotsu_ramenTests : NSObject
-@end
-@implementation PodsDummy_Pods_tonkotsu_ramenTests
-@end
