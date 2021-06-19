18e7d4eab91baba75721753a9cb30377d0d6d099
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-06-19 15:02:20 +0900

    add AlertErrorConst

---
+++ tonkotsu_ramen/Const/APIErrorMessageConst.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class APIErrorMessageConst {
+    static let mappingError = "Mapping failed."
+    static let internalErrorMessage = "The request failed due to an internal error."
+    static let serviceUnavailableError = "The server is experiencing heavy access. Please take some time to connect again."
+    static let badGatewayError = "The server encountered a temporary error and could not complete your request"
+    static let unexpectedError = "An unexpected error has occurred."
+}
