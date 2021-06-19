87a6946b20c1e1936a73a1073f0d8d5d732e13e8
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-06-19 15:02:05 +0900

    add Alert Utillity

---
+++ tonkotsu_ramen/Utillity/AlertUtil.swift
@@ -0,0 +1,26 @@
+import Foundation
+import UIKit
+
+class AlertUtil {
+    static func errorAlert(vc: UIViewController, error: Error) {
+        var title: String?
+        var message: String?
+
+        let customError = error as? ArticleCustomError
+        switch customError {
+        case .errorResponse(let error):
+            title = error.type
+            message = error.message
+        default:
+            title = "予期せぬエラーが発生しました。"
+            message = error.localizedDescription
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let close = UIAlertAction(title: "閉じる", style: .default) { (action) in
+            vc.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(close)
+        vc.present(alert, animated: true, completion: nil)
+    }
+}
