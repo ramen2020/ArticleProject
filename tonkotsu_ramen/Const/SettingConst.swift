732adcb63b8bb0fab6015dc201da5147adb94053
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-06-19 09:09:41 +0900

    add search category button const

---
+++ tonkotsu_ramen/Const/SettingConst.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class SettingConst {
+    
+    static let searchCategoryButtons: [SearchArticleCategoryButton] = [
+        SearchArticleCategoryButton(id: 1, name: "Swift"),
+        SearchArticleCategoryButton(id: 2, name: "Flutter"),
+        SearchArticleCategoryButton(id: 3, name: "Laravel"),
+        SearchArticleCategoryButton(id: 4, name: "Vue"),
+        SearchArticleCategoryButton(id: 5, name: "Docker"),
+        SearchArticleCategoryButton(id: 6, name: "AWS"),
+    ]
+
+}
