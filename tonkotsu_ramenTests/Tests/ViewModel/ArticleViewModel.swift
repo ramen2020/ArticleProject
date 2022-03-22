de8e1cbdf3170a615995284368e28fa9f7099d78
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-03-23 00:00:27 +0900

    add test

--- tonkotsu_ramenTests/Tests/ViewModel/ArticleViewModel.swift
+++ tonkotsu_ramenTests/Tests/ViewModel/ArticleViewModel.swift
@@ -8,6 +8,7 @@
 import XCTest
 import RxCocoa
 import RxSwift
+import RxTest
 
 @testable import tonkotsu_ramen
 
@@ -43,36 +44,56 @@ class ArticleViewModelTests: XCTestCase {
     
     func test_記事一覧取得() {
         let expect = expectation(description: "wait for updating apiSession article")
-        
+
         let disposable = dependency.viewModel.output.articles
             .subscribe(onNext: { articles in
                 XCTAssertEqual(articles.count, Article.mockArticles().count)
                 XCTAssertNotNil(articles.first)
                 expect.fulfill()
             })
-            
-        dependency.viewModel.featchArticles.onNext(Void())
-        wait(for: [expect], timeout: 0.1)
+
+        dependency.viewModel.searchWord.onNext("おけええええええええ")
+        wait(for: [expect], timeout: 3)
         disposable.dispose()
     }
-    
-//    func test_カテゴリの入力値に対し、記事を出力できているかどうか() {
-//        let searchArticleCategoryButtons: [SearchArticleCategoryButton] = SettingConst.searchCategoryButtons
-//
-//        let expect = expectation(description: "wait for updating apiSession article")
-//        
-//        let disposable = dependency.viewModel.output.articles
-//            .subscribe(onNext: { articles in
-//                XCTAssertEqual(articles.count, Article.mockArticles().count)
-//                XCTAssertNotNil(articles.first)
-//                expect.fulfill()
-//            })
-//            
-//        dependency.viewModel.featchArticles.onNext(Void())
-//        wait(for: [expect], timeout: 0.1)
-//        disposable.dispose()
-//        
-//        dependency.viewModel.featchArticles.onNext(Void())
-//    }
-    
+
+    func test_カテゴリの入力値に対し記事を出力できているかどうか() {
+        let searchArticleCategoryButtons: [SearchArticleCategoryButton] = SettingConst.searchCategoryButtons
+        let scheduler = TestScheduler(initialClock: 0)
+        let disposeBag = DisposeBag()
+        
+        let swiftCategory = searchArticleCategoryButtons.filter{$0.name == "Swift"}.first!
+        let flutterCategory = searchArticleCategoryButtons.filter{$0.name == "Flutter"}.first!
+        let laravelCategory = searchArticleCategoryButtons.filter{$0.name == "Laravel"}.first!
+        let vueCategory = searchArticleCategoryButtons.filter{$0.name == "Vue"}.first!
+        
+        let searchCategoryButtonTapped: TestableObservable<SearchArticleCategoryButton> = scheduler.createHotObservable([
+            Recorded.next(100, swiftCategory),
+            Recorded.next(200, flutterCategory),
+            Recorded.next(300, laravelCategory),
+            Recorded.next(400, vueCategory),
+        ])
+
+        let observer = scheduler.createObserver(Int.self)
+        
+        searchCategoryButtonTapped
+            .bind(to: dependency.viewModel.input.searchCategoryButtonTapped)
+            .disposed(by: disposeBag)
+        
+        dependency.viewModel.output.articles
+            .map{$0.count}
+            .bind(to: observer)
+            .disposed(by: disposeBag)
+        
+        let expectedEvents = [
+            Recorded.next(100, 1), // swiftの記事は1個
+            Recorded.next(200, 1), // flutterの記事は１個
+            Recorded.next(300, 0), // laravelは0個
+            Recorded.next(400, 0) // vuewは0個
+        ]
+
+        scheduler.start()
+        
+        XCTAssertEqual(observer.events, expectedEvents)
+    }
 }
